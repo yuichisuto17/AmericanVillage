6a51854a7349b30f29de660e61907276adf54cf5
Misuzu Tomari <mee.15.751@gmail.com>
2021-06-18 08:36:57 +0900

    footer php

--- wp-content/themes/amricanVillage/footer.php
+++ wp-content/themes/amricanVillage/footer.php
@@ -1,7 +1,7 @@
 <footer>
         <p>Copyright &copy; Shinohara. ALL RIGHT RESERVED.</p>
     </footer>
-    
+    <?php wp_footer(); ?>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="./assets/js/main.js"></script>
