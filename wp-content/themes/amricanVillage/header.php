7af6a25dcbdb253cfa9a103789ddf7b6ba713416
yuichi <yuyixuteng03@gmail.com>
2021-06-17 21:58:42 +0900

    ありがとー

--- wp-content/themes/amricanVillage/header.php
+++ wp-content/themes/amricanVillage/header.php
@@ -7,14 +7,17 @@
     <title>American Village</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
     <link rel="stylesheet" href="./assets/css/style.css">
-    <meta name=”viewport” content=”width=device-width, initial-scale=1”>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<body>
+<body <?php body_class();?>>
+<?php get_search_form(); ?>
     <header>
         <div class="header">
             <div class="header-left">
+            <a href="<?php echo esc_url(home_url('/'));?>"><?php bolginfo('name');?></a>
             <img src="./assets/img/logo.png" alt="logo" class="maru" width="242px" height="242px">
             </div>
+            <p class="header__descrition"><?php bloginfo( 'description' ); ?></p>
             <ul>
                 <li><a href="#menu1"></a>Menu01</li>
                 <li><a href=""></a>Menu02</li>
