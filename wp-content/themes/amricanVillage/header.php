8eb02e557952825a634bdf61ed00ca3b39059cb0
Misuzu Tomari <mee.15.751@gmail.com>
2021-06-17 20:42:47 +0900

    とりあえずここまでやっとくね

---
+++ wp-content/themes/amricanVillage/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>American Village</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <meta name=”viewport” content=”width=device-width, initial-scale=1”>
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="header-left">
+            <img src="./assets/img/logo.png" alt="logo" class="maru" width="242px" height="242px">
+            </div>
+            <ul>
+                <li><a href="#menu1"></a>Menu01</li>
+                <li><a href=""></a>Menu02</li>
+                <li><a href=""></a>Menu03</li>
+                <li><a href=""></a>Menu04</li>
+            </ul>
+        </div>
+    </header>
